d40a36e895c9f0c633cd99ff5078cfda63e936c2
Michael Peyton Jones <me@michaelpj.com>
2018-03-24 16:15:39 +0000

    More

--- src/CachingEval.hs
+++ src/CachingEval.hs
@@ -4,10 +4,9 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Simple where
@@ -59,6 +58,7 @@ testTerm :: Term
 testTerm = let x = (Fix $ MultF
                      (Fix $ IntConstF 2)
                      (Fix $ IntConstF 3))
+               -- y is content-equivalent to x, but not structurally equivalent
                y = (Fix $ MultF
                      (Fix $ PlusF (Fix $ IntConstF 1) (Fix $ IntConstF 1))
                      (Fix $ IntConstF 3))
@@ -132,15 +132,20 @@ testCache2 = do
 -- Content caching passing up caches when necessary
 
 data Caches a = Caches {
-  contentCache:: Cache a,
-  hashCache:: Cache Hash
+  contentCache :: Cache a,
+  hashHashCache :: Cache Hash,
+  treeHashCache :: Cache Hash
 } deriving (Show)
 
 type M3 a = (StateT (Caches a)) (LoggingT IO) a
 runM3 :: M3 a -> IO (a, Caches a)
-runM3 m = runStdoutLoggingT (runStateT m Caches { contentCache = M.empty, hashCache = M.empty } )
+runM3 m = runStdoutLoggingT (runStateT m Caches { contentCache = M.empty, hashHashCache = M.empty, treeHashCache = M.empty } )
 
-data Result m a = Value a | Content Hash (m a)
+data Result m a =
+  -- An actual value
+  Value a
+  -- A content hash and a computation we can run to get the value
+  | Content Hash (m a)
 instance Show a => Show (Result m a) where
   show (Value v) = show v
   show (Content h _) = show h
@@ -164,9 +169,9 @@ algebraCacheGeneric :: (MonadState (Caches a) m,
 algebraCacheGeneric algebra t = do
   let termHash = hashWithSalt1 defaultSalt $ fmap resultToHash t
   logInfoN $ "Considering " <> T.pack (show1 t) <> ", term hash is " <> T.pack (show termHash)
-  caches <- get
-  let maybeContentHash = M.lookup termHash (hashCache caches)
-  let computation = contentComputation algebra maybeContentHash t
+  hc <- gets hashHashCache
+  let maybeContentHash = M.lookup termHash hc
+  let computation = contentComputation algebra maybeContentHash (mapM resultToValue t)
   case maybeContentHash of
     Just contentHash -> do
       logInfoN $ "Hash cache hit for " <> T.pack (show termHash)
@@ -175,7 +180,7 @@ algebraCacheGeneric algebra t = do
       logInfoN $ "Hash cache miss for " <> T.pack (show termHash)
       evaled <- computation
       modify $ \c -> c {
-        hashCache = M.insert termHash (hash evaled) (hashCache c)
+        hashHashCache = M.insert termHash (hash evaled) (hashHashCache c)
       }
       pure $ Value evaled
 
@@ -186,27 +191,26 @@ contentComputation :: (MonadState (Caches a) m,
                    Traversable f,
                    Show1 f,
                    Show a) =>
-                  MAlgebra f m a -> Maybe Hash -> f (Result m a) -> m a
+                  MAlgebra f m a -> Maybe Hash -> m (f a) -> m a
 contentComputation algebra maybeContentHash t = do
-  logInfoN $ "Considering " <> T.pack (show1 t) <> ", content hash is " <> T.pack (show maybeContentHash)
-  caches <- get
+  cc <- gets contentCache
   let maybeContent = do
         contentHash <- maybeContentHash
-        M.lookup contentHash (contentCache caches)
+        M.lookup contentHash cc
   case maybeContent of
     Just value -> do
       logInfoN $ "Content cache hit for " <> T.pack (show maybeContentHash)
       pure value
     Nothing -> do
       logInfoN $ "Content cache miss for " <> T.pack (show maybeContentHash)
-      evaled <- mapM resultToValue t >>= algebra
+      evaled <- t >>= algebra
       modify $ \c -> c {
         contentCache = M.insert (hash evaled) evaled (contentCache c)
       }
       pure evaled
 
-testCache4 :: M3 Int
-testCache4 = do
+testCache3 :: M3 Int
+testCache3 = do
   res <- do
     logInfoN ("ATTEMPT 1")
     i <- cataM (algebraCacheGeneric evalAlgebra) testTerm
